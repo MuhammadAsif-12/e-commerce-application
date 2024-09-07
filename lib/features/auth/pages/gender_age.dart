8d094dc3e978878f4180288b4566b8b9645885ff
Muhammad Asif <m.asif.addi@gmail.com>
2024-09-07 22:28:42 +0500

    added gender and age selection page.

---
+++ lib/features/auth/pages/gender_age.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class GenderAndAgeSelectionPage extends StatelessWidget {
+  const GenderAndAgeSelectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
